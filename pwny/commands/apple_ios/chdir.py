2609d0d897619fc3518666e09315bafc4849ed1b
Ivan Nikolsky <enty8080@gmail.com>
2021-12-29 16:35:53 +0100

    Create chdir.py

---
+++ pwny/commands/apple_ios/chdir.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+#
+# This command requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.command import Command
+
+
+class HatSploitCommand(Command):
+    details = {
+        'Category': "filesystem",
+        'Name': "chdir",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - command developer'
+        ],
+        'Description': "Change current working directory.",
+        'Usage': "chdir <path>",
+        'MinArgs': 1
+    }
+
+    def run(self, argc, argv):
+        command = f"{argv[0]} {argv[1]}"
+        output = self.session.send_command(command, True)
+
+        if output:
+            self.print_empty(output)
