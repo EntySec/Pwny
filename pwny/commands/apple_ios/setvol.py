e8ecfdd0b2479317aa4f7809cb25872c067d0697
Ivan Nikolsky <enty8080@gmail.com>
2021-12-29 16:44:44 +0100

    Create setvol.py

---
+++ pwny/commands/apple_ios/setvol.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#
+# This command requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.command import Command
+
+
+class HatSploitCommand(Command):
+    details = {
+        'Category': "misc",
+        'Name': "setvol",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - command developer'
+        ],
+        'Description': "Set volume level.",
+        'Usage': "setvol {0-10}",
+        'MinArgs': 1
+    }
+
+    def run(self, argc, argv):
+        if argv[1].isdigit():
+            level = int(argv[1])
+
+            if level in range(0, 10):
+                command = f"{argv[0]} {str(level / 10)}"
+
+                self.session.send_command(command)
+                return
+
+         self.print_usage(self.details['Usage'])
