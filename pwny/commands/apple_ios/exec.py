ea5304db2bd6aa68489c21f146d17d8b16f6f3ca
Ivan Nikolsky <enty8080@gmail.com>
2021-12-29 16:31:58 +0100

    Create exec.py

---
+++ pwny/commands/apple_ios/exec.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+#
+# This command requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.command import Command
+
+
+class HatSploitCommand(Command):
+    details = {
+        'Category': "misc",
+        'Name': "exec",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - command developer'
+        ],
+        'Description': "Execute system command.",
+        'Usage': "exec <command>",
+        'MinArgs': 0
+    }
+
+    def run(self, argc, argv):
+        command = f"{argv[0]} {argv[1]}"
+        output = self.session.send_command(command, True)
+
+        if output:
+            self.print_empty(output)
