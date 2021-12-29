fc88e48fc3c91d49d55d7312abf03e5ec7e9f96c
Ivan Nikolsky <enty8080@gmail.com>
2021-12-29 16:25:07 +0100

    Create getpaste.py

---
+++ pwny/commands/apple_ios/getpaste.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#
+# This command requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.command import Command
+
+
+class HatSploitCommand(Command):
+    details = {
+        'Category': "gather",
+        'Name': "getpaste",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - command developer'
+        ],
+        'Description': "Show PasteBoard contents.",
+        'Usage': "pasteboard",
+        'MinArgs': 0
+    }
+
+    def run(self, argc, argv):
+        self.session.send_command(argv[0])
