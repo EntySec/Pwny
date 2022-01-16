9dcdfffbcd8867470b3fceeae1bc3b6aaa9a4a27
Ivan Nikolsky <enty8080@gmail.com>
2022-01-16 04:04:04 +0300

    Update and rename inline.c to template.c

--- src/pwny/template.c
+++ src/pwny/template.c
@@ -51,7 +51,11 @@ int main(int argc, char *argv[])
     char *host = find_json(json, "host");
     char *port = find_json(json, "port");
 
-    int channel = open_channel(host, atoi(port));
+    if (!host)
+        int channel = listen_channel(atoi(port));
+    else
+        int channel = open_channel(host, atoi(port));
+
     if (channel < 0)
         return -1;
 
