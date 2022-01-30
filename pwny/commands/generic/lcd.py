3a2af40978895733cc16958939e4bbfb8ccb8bdd
Ivan Nikolsky <enty8080@gmail.com>
2022-01-30 14:59:36 +0300

    Create lcd.py

---
+++ pwny/commands/generic/lcd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+#
+# This command requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+import os
+
+from hatsploit.lib.command import Command
+
+
+class HatSploitCommand(Command):
+    details = {
+        'Category': "local",
+        'Name': "lchdir",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - command developer'
+        ],
+        'Description': "Change current directory.",
+        'Usage': "lchdir",
+        'MinArgs': 1
+    }
+
+    def run(self, argc, argv):
+        os.chdir(argv[1])
+        self.print_information(f"Current directory: {os.getcwd()}")
