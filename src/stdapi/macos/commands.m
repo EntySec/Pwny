dca38f160f232f894573754bd0b7c9b4a2ad1f7a
Ivan Nikolsky <enty8080@gmail.com>
2022-03-04 08:23:03 +0000

    Update commands.m

--- src/stdapi/macos/commands.m
+++ src/stdapi/macos/commands.m
@@ -42,4 +42,29 @@
     send_channel(channelPipe, (char *)[[NSString stringWithFormat:@"%sPID: %d", information, processID] UTF8String]);
 }
 
+-(void)cmd_idletime {
+    int64_t idlesecs = -1;
+    io_iterator_t iter = 0;
+    if (IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching("IOHIDSystem"), &iter) == KERN_SUCCESS) {
+        io_registry_entry_t entry = IOIteratorNext(iter);
+        if (entry) {
+            CFMutableDictionaryRef dict = NULL;
+            if (IORegistryEntryCreateCFProperties(entry, &dict, kCFAllocatorDefault, 0) == KERN_SUCCESS) {
+                CFNumberRef obj = CFDictionaryGetValue(dict, CFSTR("HIDIdleTime"));
+                if (obj) {
+                    int64_t nanoseconds = 0;
+                    if (CFNumberGetValue(obj, kCFNumberSInt64Type, &nanoseconds)) {
+                        idlesecs = (nanoseconds >> 30);
+                    }
+                }
+                CFRelease(dict);
+            }
+            IOObjectRelease(entry);
+        }
+        IOObjectRelease(iter);
+    }
+    send_channel(channelPipe, (char *)[[NSString stringWithFormat:@"%sUser has been idle for %lld seconds.",
+                                        information, idlesecs] UTF8String]);
+}
+
 @end
