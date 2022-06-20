5180dbeb340d9b78f31a2b6ef02aeda343dd0e46
Ivan Nikolsky <enty8080@gmail.com>
2022-06-20 22:49:40 +0200

    Update commands.m

--- src/stdapi/apple_ios/commands.m
+++ src/stdapi/apple_ios/commands.m
@@ -39,6 +39,14 @@
     return self;
 }
 
+-(void)execute_plugin:(NSString *)plugin withCmd:(NSString *)cmd withArgs:(NSString *)args {
+    CPDistributedMessagingCenter *messagingCenter = [CPDistributedMessagingCenter centerNamed:plugin];
+    NSDictionary *userInfo = [NSDictionary dictionaryWithObject:cmd forKey:@"cmd"];
+    NSDictionary* reply = [_messagingCenter sendMessageAndReceiveReplyName:@"recieveCommand" userInfo:userInfo];
+    NSString* result = [reply objectForKey:@"returnStatus"];
+    send_channel(channelPipe, (char *)[result UTF8String]);
+}
+
 -(void)cmd_sysinfo {
     UIDevice* device = [UIDevice currentDevice];
     int batinfo = ([_thisUIDevice batteryLevel] * 100);
