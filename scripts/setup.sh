70890c99845e52d7103b2d70c53f8fc0c1b26e9f
Ivan Nikolsky <enty8080@gmail.com>
2023-04-21 22:18:43 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,13 @@ export AR=$3
 
 cd deps
 
+echo "[Building elf2bin]"
+
+gcc -o elf2bin elf2bin.c
+cp elf2bin ../
+
+echo "[Done building elf2bin]"
+
 echo "[Installing kubo/injector]"
 
 git clone https://github.com/enty8080/injector
