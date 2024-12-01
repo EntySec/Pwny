6bbb26aace5aa515c006a1942a12f227472cca22
Ivan Nikolskiy <enty8080@gmail.com>
2024-12-01 04:29:52 +0000

    Update pwny_reverse_tcp.py

--- payload/linux/mipsbe/pwny_reverse_tcp.py
+++ payload/linux/mipsbe/pwny_reverse_tcp.py
@@ -22,10 +22,10 @@ class HatSploitPayload(Payload, Handler):
             'Authors': [
                 "Ivan Nikolskiy (enty8080) - payload developer",
             ],
-            'Description': (
-                "This payload creates an interactive reverse Pwny shell for Linux "
-                "with MIPS big-endian architecture."
-            ),
+            'Description': """
+                This payload creates an interactive reverse Pwny shell for Linux
+                with MIPS big-endian architecture.
+            """,
             'Arch': ARCH_MIPSBE,
             'Platform': OS_LINUX,
             'Session': PwnySession,
